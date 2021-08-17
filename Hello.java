21bee6e978b47cb052c2180f4225ac940056e402
divya <divyakayutham@gmail.com>
2021-08-17 05:51:42 +0000

    Rectifying the mistake

--- Hello.java
+++ Hello.java
@@ -2,10 +2,7 @@ class Hello
 {
  public static void main(string args[])
 	{
-<<<<<<< HEAD
+
 	  system.out.println("Hello from master");
-=======
-	  system.out.println("Hello from feature101");
->>>>>>> feature101
 	}
 }
